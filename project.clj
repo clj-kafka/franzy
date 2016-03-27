a4b99bb3284f2617f0bbc1616d9da513f35babd1
ymilky <yossimilky@gmail.com>
2016-03-27 16:46:31 -0400

    version bump to 0.0.2-snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ymilky/franzy-examples "0.0.1-SNAPSHOT"
+(defproject ymilky/franzy-examples "0.0.2-SNAPSHOT"
   :description "Examples for Franzy"
   :url "https://github.com/ymilky/franzy-examples"
   :author "ymilky"
@@ -15,14 +15,14 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/core.async "0.2.374"]
                  [com.taoensso/timbre "4.3.1"]
-                 [ymilky/travel-zoo "0.0.1"]
+                 [ymilky/travel-zoo "0.0.2"]
                  [jarohen/nomad "0.7.2"]
-                 [ymilky/franzy "0.0.1-SNAPSHOT"]
-                 [ymilky/franzy-admin "0.0.1-SNAPSHOT"]
-                 [ymilky/franzy-nippy "0.0.1-SNAPSHOT"]
-                 [ymilky/franzy-fressian "0.0.1-SNAPSHOT"]
-                 [ymilky/franzy-json "0.0.1-SNAPSHOT"]
-                 [ymilky/franzy-embedded "0.0.1-SNAPSHOT"]
+                 [ymilky/franzy "0.0.2-SNAPSHOT"]
+                 [ymilky/franzy-admin "0.0.2-SNAPSHOT"]
+                 [ymilky/franzy-nippy "0.0.1"]
+                 [ymilky/franzy-fressian "0.0.1"]
+                 [ymilky/franzy-json "0.0.1"]
+                 [ymilky/franzy-embedded "0.0.2-SNAPSHOT"]
                  [org.slf4j/slf4j-api "1.7.19"]
                  [org.slf4j/slf4j-nop "1.7.19"]
                  [log4j/log4j "1.2.17"]]
